9a64bce90416fb9c73cfe8009cf4c8dc05e03bdf
Aliazhari <Aliazhari@hotmail.com>
2020-01-21 02:03:41 -0500

    script_08

---
+++ script_08.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#  Author : Ali Azhari 
+#  Created On : Tue Jan 21 2020
+#  File : script_08.sh
+# ****************************************** 
+
+ 	# Define function
+ 	current_date() {
+ 		
+ 		# Get current date and time
+ 		date
+ 		
+ 		# returns an exit status number between 0 - 255
+ 		return
+ 	}
+ 	
+ 	current_date
