d388b098c8e7e7252219d4734cfa5ecc870ec2ef
Aliazhari <Aliazhari@hotmail.com>
2020-01-21 01:24:33 -0500

    script_05

---
+++ script_05.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#  Author : Ali Azhari 
+#  Created On : Tue Jan 21 2020
+#  File : script_05.sh
+# ****************************************** 
+
+# Shorthand increment and decrement
+    num=10
+	echo "num++ = $(( num++ ))"
+	echo "++num = $(( ++num ))"
+	echo "num-- = $(( num-- ))"
+	echo "--num = $(( --num ))"
+  
