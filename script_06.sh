19e5df9577be51c6e8788510a226cd6755eb53f1
Aliazhari <Aliazhari@hotmail.com>
2020-01-21 01:44:48 -0500

    script_06

---
+++ script_06.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#  Author : Ali Azhari 
+#  Created On : Tue Jan 21 2020
+#  File : script_06.sh
+# ****************************************** 
+
+# Using Python to operate on float numbers.
+    num1=1.2
+    num2=2.5
+    num3=$(python -c "print $num1+$num2")
+    echo $num3
