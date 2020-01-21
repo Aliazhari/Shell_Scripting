7a10eaee2d9514089f1ee51c325136cda4e81c7a
Aliazhari <Aliazhari@hotmail.com>
2020-01-21 01:56:00 -0500

    script_07

---
+++ script_07.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#  Author : Ali Azhari 
+#  Created On : Tue Jan 21 2020
+#  File : script_07.sh
+# ****************************************** 
+
+# Printing multiple lines using cat
+# cat prints a file or any string past to it
+	
+cat << END
+This line will be printed exactly in the same
+format as it is in this file
+	
+END
