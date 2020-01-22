7cd0ed8479d5793c9e42820ac65db0a82b7e8485
Aliazhari <Aliazhari@hotmail.com>
2020-01-21 21:18:50 -0500

    script_11 - Using command read and conditions

---
+++ script_11.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#  Author : Ali Azhari 
+#  Created On : Tue Jan 21 2020
+#  File : script_11.sh
+# ****************************************** 
+
+	
+# use command "read" to get input from user
+
+    declare -r USER="Ali"
+    declare -r PASS=1234
+
+  	read -p "What is your username? " username
+  	read -p "What is your passcode" passcode
+    
+# Conditions should be within []
+# Include a space after [ and before ]
+# Integer Comparisons: eq, ne, le, lt, ge, gt
+
+
+    if [ $username == $USER ] && [ $passcode -eq $PASS ]
+    then
+        echo "Congratulations!!"
+    else
+        echo "Too bad!!"
+    fi
