8d66eb8df19146abcc3d8af062128eba075acc1d
Aliazhari <Aliazhari@hotmail.com>
2020-01-24 13:57:39 -0500

    script_14 for loop

---
+++ script_14.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#  Author : Ali Azhari 
+#  Created On : Fri Jan 24 2020
+#  File : script_14.sh
+# ****************************************** 
+
+# for loop
+
+for (( i=0; i <= 20; i++ )); do
+  		echo $i
+  	done
