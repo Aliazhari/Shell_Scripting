6ab4ec15dc6cf3446199092849f957bb534515ef
Aliazhari <Aliazhari@hotmail.com>
2020-01-24 19:12:35 -0500

    "script_18.sh - Arguments"

---
+++ script_18.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#  Author : Ali Azhari 
+#  Created On : Fri Jan 24 2020
+#  File : script_18.sh
+# ****************************************** 
+
+# Print the first argument
+
+    echo "The number of arguments: " $#
+	echo "1st Argument : $1"
